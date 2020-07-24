668d2dfe80f31aecfcd4abd5db84dccd19465967
cloudacademy-A <cloudacademy.101@gmail.com>
2020-07-24 22:03:30 +0530

    Login Page Design

--- src/LoginComponents/LoginComponent.jsx
+++ src/LoginComponents/LoginComponent.jsx
@@ -8,7 +8,26 @@ export default class LoginComponent extends Component {
 
     render() {
         return (
-            <div>This is Login Component</div>
+            <div>
+            <div className='header-box'>
+                Login With Your Mobile Number
+            </div>
+            <div className='details-block-left'>
+          
+                <div className='textbox'>
+                    <span className='left-span'>Mobile Number</span>
+                    <span><input type='text' maxLength='10' /></span>
+                </div>
+                <div className='textbox'>
+                    <span className='left-span'>OTP</span>
+                    <span><input type='text' maxLength='6' /></span>
+                </div>
+                
+                <div className='submit'>
+                    <button className='btn'>Log in</button>
+                </div>
+            </div>
+        </div>
         )
     }
 }
