fa42eb12ca8f97a09a55037f10986d56a74597e5
SRoy <chatterjeeavishek89@gmail.com>
2020-07-29 02:36:36 +0530

    changes for covid tracker list component and package dependency

--- src/CovidComponents/CovidSuspectList.jsx
+++ src/CovidComponents/CovidSuspectList.jsx
@@ -1,29 +1,71 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import covidData from '../JSONdb/COVIDTracker.json';
+
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
 
 class CovidSuspectList extends Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            covidSuspectList: [],
+            isGovtEmployee: true
+        }
+    }
+
+    componentDidMount() {
+        if (covidData.length > 0) {
+            const covidSuspectList = covidData.filter((row) => {
+                return row.COVIDSuspect === true;
+            });
+            this.setState({ covidSuspectList });
+        }
     }
 
     render() {
-       
+
+        const { covidSuspectList, isGovtEmployee } = this.state;
         return (
-            <div>
-                Suspect List
-            </div>
+            <>
+                {covidSuspectList.length > 0 && covidSuspectList.map((row) => {
+                    return (
+                        <Row key={row.id}>
+                            <Col xs={12} sm={6} style={{ height: '10rem' }}>
+                                <Card>
+                                    <Card.Body>
+                                        <Card.Title>{row.PatientDetails.PatientName}</Card.Title>
+                                        <Card.Subtitle className="mb-2 text-muted">Covid Status - {row.COVIDConfirm ? 'Confirmed' : 'Not Confirmed'}</Card.Subtitle>
+                                        <Card.Text>
+                                            Pin - {row.PatientDetails.PinCode}
+                                        </Card.Text>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                            <Col xs={12} sm={5} className="mt-4">
+                            {isGovtEmployee && 
+                                <Button variant="primary">Update</Button>
+                            }
+                            </Col>
+                        </Row>
+                    )
+
+                })}
+            </>
         )
     }
 }
 
 const mapStateToProps = state => {
     console.log('state', state);
-   
+
 }
 
 const mapDispatchToProps = dispatch => {
-   
+
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(CovidSuspectList);
