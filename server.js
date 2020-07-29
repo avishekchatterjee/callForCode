a303ca8bd26d5ba19aae71942a425cc113f554d5
cloudacademy-A <cloudacademy.101@gmail.com>
2020-07-29 18:04:11 +0530

    Try Changes for Deployment

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const app = express(); // create express app
 const path = require('path')
+var bodyParser = require('body-parser')
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 app.use(express.static(path.join(__dirname, 'build')))
 
