d14a7e945f09fbc3a235b38f384d205b2c423486
SRoy <chatterjeeavishek89@gmail.com>
2020-07-31 22:08:27 +0530

    chat bot

--- src/ChatBot/ChatBot.jsx
+++ src/ChatBot/ChatBot.jsx
@@ -7,19 +7,30 @@ export default class ChatBot extends Component {
         super(props)
         this.state = {
             chatContainerShow: false,
-            inputValue: ''
+            inputValue: '',
+            chatContent: '',
+            resetValue: '',
+            loading: false
         }
     }
 
     showChatContainer() {
+        const { chatContent, resetValue, loading } = this.state;
         return (
             <div className="chatbot-container">
-                <div className="chatbot-result-area">
-
+                <div className="chatbot-result-area" dangerouslySetInnerHTML={{ __html: chatContent }}>
                 </div>
                 <div className="chatbot-input-area">
-                    <textarea className="input-chatbot" name="chatinput" onChange={(e) => this.handleInputOnchange(e)}></textarea>
-                    <button className="btn-primary send-btn" onClick={this.handleChatSend}>SEND</button>
+                    {
+                        loading && <span className="watson-loading">Watson thinking...</span>
+                    }
+                    {
+                        !loading &&  <>
+                            <input className="input-chatbot" name="chatinput" onChange={(e) => this.handleInputOnchange(e)} value={resetValue} />
+                            <button className="btn-primary send-btn" onClick={this.handleChatSend}>SEND</button>
+                        </>
+                    }
+
                 </div>
 
             </div>
@@ -28,13 +39,23 @@ export default class ChatBot extends Component {
 
     handleInputOnchange(e) {
         const value = e.target.value;
-        this.setState({ inputValue: value });
+        this.setState({ inputValue: value, resetValue: value });
     }
 
     handleChatSend = () => {
         const { inputValue } = this.state;
         if (inputValue.trim() !== '') {
-            console.log('ssss')
+
+            this.setState((prevState) => {
+                return {
+                    chatContent: prevState.chatContent + `<p class="user-text">${inputValue}</p>`,
+                    resetValue: '',
+                    inputValue: ''
+                }
+            });
+
+
+
             const payload = {
                 "input": {
                     "text": inputValue
@@ -42,27 +63,55 @@ export default class ChatBot extends Component {
                 "alternate_intents": true
             }
 
-            API.post('https://api.eu-gb.assistant.watson.cloud.ibm.com/instances/e082c043-42fe-4831-ab3c-3b69af080ad3/v1/workspaces/2301ce51-c1a2-4061-af05-151bc8fdc5ac/message?version=2020-04-01',
-             payload, {auth: {
-                username: 'apikey',
-                password: 'YXBpa2V5OmtiZGQ1YzFVWkZkXzl5bTJlSWtQTlRrQ1ZhWWgtRWVQN1dlYjBvYlc0MEpq'
-            }}).then((data)=>{
-                console.log(data)
-            });
+            this.setState({loading: true});
+
+            API.post('api/ChatBot/stateWiseData',
+                payload).then((resp) => {
+                    let result = '<ul class="reply-divs">';
+                    if (resp && resp.data && resp.data.value.output && resp.data.value.output.text.length > 0) {
+                        const respArr = resp.data.value.output.text;
+                        respArr.map((row) => {
+                            result = result + `<li>${row}</li>`
+                        });
+                    } else {
+                        console.log('no answer')
+                        result = result + `<li>Sorry, I'm not ready for this question</li>`
+                    }
+                    result = result + `</ul>`;
+                    this.setState((prevState) => {
+                        return {
+                            chatContent: prevState.chatContent + `<div class="bot-text"><span class="watson-label-resp">Watson :</span>${result}</div>`
+                        }
+                    });
+
+                }).catch((e) => {
+                    console.log('error')
+                    this.setState((prevState) => {
+                        return {
+                            chatContent: prevState.chatContent + `<div class="bot-text"><span class="watson-label-resp">Watson :</span><ul class="reply-divs"><li>Sorry can't fetch answer for you right now. Please try later...</li></ul></div>`
+                        }
+                    });
+                }).finally(() => {
+                    const ele = document.querySelector(".chatbot-result-area");
+                    ele.scrollTop = ele.scrollHeight;
+                    this.setState({loading: false});
+                });
         }
     }
 
     render() {
         return (
             <div className="chat-div-main">
-                <div className="chat-lower-bar" onClick={
-                    (e) => {
-                        e.preventDefault();
-                        this.setState((prevState) => {
-                            return { chatContainerShow: !prevState.chatContainerShow }
-                        })
-                    }}>
-                    Click To Know About Covid19
+                <div className="chat-lower-bar">
+                    All About Covid 19 {!this.state.chatContainerShow && <span className="chatbot-icon" onClick={
+                        (e) => {
+                            e.preventDefault();
+                            this.setState({ chatContainerShow: true });
+                        }}>^</span>}{this.state.chatContainerShow && <span className="chatbot-icon" onClick={
+                            (e) => {
+                                e.preventDefault();
+                                this.setState({ chatContainerShow: false, chatContent: '', inputValue: '' });
+                            }}>x</span>}
                 </div>
                 {this.state.chatContainerShow && this.showChatContainer()}
             </div>
--- src/services/API.jsx
+++ src/services/API.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    
+    baseURL: 'https://cfc2020apis.azurewebsites.net/'
 });
 
 export default API;
